53345eeaef206ce53c518675454a6235cf6e4078
R2D2 <artur0y95@gmail.com>
2025-10-26 21:48:22 +0100

    zadeklarowano zmienne o stanie pogody, zainicjalizowano obiekt skanera, napisano instrukcje przyjmującą odpowiedź użytkownika

--- cw02/src/zadanie2/Pogoda.java
+++ cw02/src/zadanie2/Pogoda.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class Pogoda {
     void main() {
-    
+        String weatherStatus;
+        Scanner input = new Scanner(System.in);
+        
+        IO.println("Jaka jest pogoda za oknem?");
+        weatherStatus = input.nextLine();
+        IO.println("Pogoda jest " + weatherStatus);
     }
 }
