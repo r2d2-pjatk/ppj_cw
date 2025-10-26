036f755335a869050487378b8b6109b2573a32a1
R2D2 <artur0y95@gmail.com>
2025-10-26 22:43:39 +0100

    dodano instrukcje wyświetlające ten sam int w postaci dec, oct, hex i bin

--- cw03/src/zadanie1/Main.java
+++ cw03/src/zadanie1/Main.java
@@ -5,5 +5,12 @@ public class Main {
         // wszystkie literały logiczne
         System.out.println(true);
         System.out.println(false);
+        
+        /* literały tego samego inta, w postaciach:
+        - dec, oct, hex, bin */
+        System.out.println(314);
+        System.out.println(0472);
+        System.out.println(0x13a);
+        System.out.println(0b100111010);
     }
 }
