f9fbdcac6f07400787a7c1a2f7519352097076bd
R2D2 <artur0y95@gmail.com>
2025-10-26 21:29:51 +0100

    wyświetlono komunikat typu Hello World wypisując słowa w oddzielnej instrukcji

---
+++ cw02/src/zadanie1/Main.java
@@ -0,0 +1,12 @@
+package zadanie1;
+
+public class Main {
+    void main() {
+        IO.print("Hello ");
+        IO.println(" World!");
+        IO.print("My ");
+        IO.print("name ");
+        IO.print("is ");
+        IO.println("Java");
+    }
+}
