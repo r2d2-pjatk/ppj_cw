f1d802c74c8ff684b072d0ce54de552fc2f6f276
R2D2 <artur0y95@gmail.com>
2025-10-27 21:52:07 +0100

    dodano instrukcje wyświetlające literał int z i bez wizualnego separatora _

---
+++ cw03/src/zadanie2/Main.java
@@ -0,0 +1,10 @@
+package zadanie2;
+
+
+public class Main {
+    void main() {
+        // ten sam literał typu int, _ poprawia czytelność
+        IO.println(1000000);
+        IO.println(1_000_000);
+    }
+}
