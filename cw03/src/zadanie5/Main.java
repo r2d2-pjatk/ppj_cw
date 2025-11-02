5a53b31b6a64ac963c5a4b399212ba7252f9eb0a
R2D2 <artur0y95@gmail.com>
2025-11-02 18:59:09 +0100

    zadeklarowano zmienne i zainicjalizowano je odpowiednimi literałami

---
+++ cw03/src/zadanie5/Main.java
@@ -0,0 +1,22 @@
+package zadanie5;
+
+public class Main {
+    public static void main(String[] args) {
+        // Zadeklaruj zmienne odpowiednich typów, zainicjalizowane podanymi literałami.
+        double a = .2;
+        double b = 16.3;
+        int c = 178;
+        long d = 4L;
+        double e = 567d;
+        float f = 15.87F;
+        boolean g = false;
+        String h = "PPJ";
+        char i = 'A';
+        int j = 0b1_0101;
+        int k = 0b10101;
+        int l = 0120;
+        int m = 0x2F;
+        char n = '\u0041';
+        double o = 0.031415e2;
+    }
+}
