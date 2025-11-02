dd93a6eb8d92243e5c17167fc435d78c21121fe1
R2D2 <artur0y95@gmail.com>
2025-11-02 19:08:34 +0100

    zadeklarowano zmienne i zainicjalizowano je odpowiednimi literałami cz.2

---
+++ cw03/src/zadanie6/Main.java
@@ -0,0 +1,22 @@
+package zadanie6;
+
+public class Main {
+    static void main() {
+        // Zadeklaruj zmienne odpowiednich typów, zainicjalizowane podanymi literałami.
+        float a = .5f;
+        int b = 202;
+        double c = 20.24e2;
+        char d = 'L';
+        double e = 789.;
+        long f = 0123L;
+        String g = "true";
+        String h = "E";
+        int i = 0xfff;
+        int j = 0b10_01;
+        int k = 10_01;
+        int l = 010;
+        double m = 404d;
+        char n = '\u0064';
+        double o = 314159e-5;
+    }
+}
