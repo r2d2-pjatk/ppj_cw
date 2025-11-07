0a4a33026c718caeaa03687aee578c01927e7f3d
R2D2 <artur0y95@gmail.com>
2025-11-07 19:35:09 +0100

    napisano program wczytujący 3 liczby naturalne i wyświetlający czy 2/3 z nich są równe lub nie

---
+++ cw04powt/src/zadanie4/Main.java
@@ -0,0 +1,23 @@
+package zadanie4;
+
+import java.util.Scanner;
+
+
+public class Main {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+        
+        IO.println("Podaj trzy liczby naturalne: ");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        
+        boolean areTwoOfThreeEqual = ((a == b) & (a != c)) || ((a == c) & (a != b)) || ((b == c) & (b != a));
+        
+        if (areTwoOfThreeEqual)
+            IO.println("OK");
+        else
+            IO.println("NOT OK");
+    }
+}
