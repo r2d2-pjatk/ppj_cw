de7943d6a72d50824bc73d8cfcc2b3e07f0d1856
R2D2 <artur0y95@gmail.com>
2025-11-07 19:40:02 +0100

    rozwiązanie konfliktu w cw04powt/src/zadanie3/Main.java

--- cw04powt/src/zadanie3/Main.java
+++ cw04powt/src/zadanie3/Main.java
@@ -13,11 +13,7 @@ public class Main {
         IO.print("Jaki jest twój wzrost (w metrach)?: ");
         height = input.nextDouble();
         
-<<<<<<< HEAD
-        IO.print("\nJaka jest twoja waga (w kilogramach)?: ");
-=======
         IO.print("Jaka jest twoja waga (w kilogramach)?: ");
->>>>>>> 8e381ce (napisano program obliczający współczynnik BMI, wyświetlając go w oknie dialogowym)
         weight = input.nextDouble();
         
         bmi = weight / Math.pow(height, 2);
