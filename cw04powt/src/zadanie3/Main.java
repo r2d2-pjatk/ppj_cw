59f12bacf8703b07f9ab8a9ed4d7707cc6606fc2
R2D2 <artur0y95@gmail.com>
2025-11-07 18:03:10 +0100

    napisano program obliczający współczynnik BMI, wyświetlając go w oknie dialogowym

---
+++ cw04powt/src/zadanie3/Main.java
@@ -0,0 +1,22 @@
+package zadanie3;
+import javax.swing.JOptionPane;
+import java.awt.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Main {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        double height, weight, bmi;
+        
+        IO.print("Jaki jest twój wzrost (w metrach)?: ");
+        height = input.nextDouble();
+        
+        IO.print("\nJaka jest twoja waga (w kilogramach)?: ");
+        weight = input.nextDouble();
+        
+        bmi = weight / Math.pow(height, 2);
+        JOptionPane.showMessageDialog(null, "Twój współczynnik BMI wynosi " + bmi);
+    }
+}
