30f31b27477901684f69049bd39f1aece89d1592
R2D2 <artur0y95@gmail.com>
2025-12-10 00:01:24 +0100

    w utworzonej klasie OneLiners zdefiniowano jednoliniowe zwracające metody statyczne

---
+++ cw08/src/zadanie1/OneLiners.java
@@ -0,0 +1,59 @@
+package zadanie1;
+
+public class OneLiners {
+    static void main(String[] args) {
+        double a = 2, b = 3, c = 1;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): hasTwoRoots? " +
+                hasTwoRoots(a,b,c));
+        
+        a = 0; b = 2; c = 1;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): hasTwoRoots? " +
+                hasTwoRoots(a,b,c));
+        
+        a = 2; b = 1; c = -1;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): monot? " +
+                monot(a,b,c));
+        
+        a = 2; b = 1; c = 2;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): monot? " +
+                monot(a,b,c));
+        
+        a = 2; b = 1; c = 2;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): maxEl = " +
+                maxEl(a,b,c));
+        
+        a = 2; b = 2; c = 4;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): maxEl = " +
+                maxEl(a,b,c));
+        
+        a = -2; b = 1; c = -3;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): numPos = " +
+                numPos(a,b,c));
+        
+        a = -2; b = 1; c = 3;
+        System.out.println("(a, b, c,)=(" + a + ", " +
+                b + ", " + c + "): numPos = " +
+                numPos(a,b,c));
+    }
+    
+    public static boolean hasTwoRoots(double a, double b, double c) {
+        // warunki: a != 0, delta > 0
+        return (a != 0) && ((b * b - 4 * a * c) > 0);
+    }
+    public static boolean monot(double a, double b, double c) {
+        return (b > a && c > b) || (a > b & b > c);
+    }
+    public static double maxEl(double a, double b, double c) {
+        return (a > b && a > c) ? a : ((b > a && b > c) ? b : c);
+    }
+    public static int numPos(double a, double b, double c) {
+        return (a > 0 ? 1 : 0) + (b > 0 ? 1 : 0) + (c > 0 ? 1 : 0);
+    }
+}
