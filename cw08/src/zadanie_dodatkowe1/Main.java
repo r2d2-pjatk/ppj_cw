25216aeec85ca9aadb543f234191adba398ca048
R2D2 <artur0y95@gmail.com>
2025-12-10 19:27:40 +0100

    utworzono program symulujący rzut 2 kostkami i z metodą zwracającą wynik 6/6, program wyświetla też procent symulacji z wynikiem 6/6 w pierwszym rzucie oraz po 128 lub więcej rzutach

---
+++ cw08/src/zadanie_dodatkowe1/Main.java
@@ -0,0 +1,56 @@
+package zadanie_dodatkowe1;
+
+public class Main {
+    static void main() {
+        /*
+        Stwórz statyczną metodę private static int roll() { // ...}
+        która przeprowadza symulację rzutu dwiema kostkami i powtarza ją, aż wypadną dwie szóstki;
+        metoda zwraca liczbę wykonanych rzutów.
+        W metodzie main wywołaj metodę roll 3 miliony razy i odpowiedz na pytania:
+        - w jakim procencie przypadków uzyskano dwie szóstki już w pierwszym rzucie;
+        - w jakim procencie przypadków uzyskano dwie szóstki dopiero po 128 lub więcej rzutach.
+        Wynik programu mógłby zatem wyglądać tak
+        [Jak można łatwo policzyć, wartości oczekiwane dla tych dwóch liczb wynoszą, odpowiednio, 2.78% i 2.79%]
+        */
+        int numOfRollCalls = 3_000_000;
+        int[] rollCountArr = new int[numOfRollCalls];
+        
+        for (int i = 0; i < rollCountArr.length; i++) {
+            rollCountArr[i] = roll();
+        }
+        
+        int pairOfSixInFirstRollCount = 0;
+        for (int val : rollCountArr) {
+            if (val == 1)
+                pairOfSixInFirstRollCount++;
+        }
+        double pairOfSixInFirstRoll = (double) pairOfSixInFirstRollCount / numOfRollCalls * 100;
+        System.out.print("In first roll:     ");
+        System.out.printf("%.2f", pairOfSixInFirstRoll);
+        System.out.println("%");
+        
+        int pairOfSixIn128orMoreRollsCount = 0;
+        for (int val : rollCountArr) {
+            if (val >= 128)
+                pairOfSixIn128orMoreRollsCount++;
+        }
+        double pairOfSixIn128orMoreRolls = (double) pairOfSixIn128orMoreRollsCount / numOfRollCalls * 100;
+        System.out.print("128 or more rolls: ");
+        System.out.printf("%.2f", pairOfSixIn128orMoreRolls);
+        System.out.println("%");
+    }
+    
+    private static int roll() {
+        int firstThrow, secondThrow;
+        int rollCount = 0;
+        
+        do {
+            firstThrow = (int) (Math.random() * 6 + 1);
+            secondThrow = (int) (Math.random() * 6 + 1);
+            System.out.println("Rzut 1: " + firstThrow + ", Rzut 2: " + secondThrow);
+            rollCount++;
+        } while (!(firstThrow == 6 && secondThrow == 6));
+        
+        return rollCount;
+    }
+}
