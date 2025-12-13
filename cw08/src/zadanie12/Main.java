5fb54346bf05834fcfd26a96cf4d9f83290e96c1
R2D2 <artur0y95@gmail.com>
2025-12-13 19:30:43 +0100

    napisano program który z tablicy 2d wyników liczy liczbę punktów każdej drużyny

---
+++ cw08/src/zadanie12/Main.java
@@ -0,0 +1,59 @@
+package zadanie12;
+
+import java.util.Arrays;
+
+public class Main {
+    static void main() {
+        String[][] arr = {
+            {"Germany",  "2", "Scotland", "1"},
+            {"Poland",   "2", "Germany",  "0"},
+            {"Germany",  "1", "Ireland",  "1"},
+            {"Poland",   "2", "Scotland", "2"},
+            {"Scotland", "1", "Ireland",  "0"},
+            {"Ireland",  "1", "Poland",   "1"},
+            {"Ireland",  "1", "Scotland", "1"},
+            {"Germany",  "3", "Poland",   "1"},
+            {"Scotland", "2", "Germany",  "3"},
+            {"Ireland",  "1", "Germany",  "0"},
+            {"Scotland", "2", "Poland",   "2"},
+            {"Poland",   "2", "Ireland",  "1"}
+        };
+        
+        String[] teams = {"Germany", "Ireland", "Poland", "Scotland"};
+        int[] totalPointsPerTeam = new int[teams.length];
+        
+        for (int i = 0; i < teams.length; i++) {
+            totalPointsPerTeam[i] = getPoints(arr, teams[i]);
+        }
+        
+        System.out.println(Arrays.toString(totalPointsPerTeam));
+    }
+    
+    public static int getPoints(String[][] results, String team) {
+        int sumOfPoints = 0;
+        
+        for (int i = 0; i < results.length; i++) {
+            String homeTeam = results[i][0];
+            String awayTeam = results[i][2];
+            int homeTeamScore = Integer.parseInt(results[i][1]);
+            int awayTeamScore = Integer.parseInt(results[i][3]);
+            boolean isHomeTeam = homeTeam.equals(team);
+            boolean isAwayTeam = awayTeam.equals(team);
+            boolean homeTeamWon = homeTeamScore > awayTeamScore;
+            boolean awayTeamWon = homeTeamScore < awayTeamScore;
+            boolean draw = homeTeamScore == awayTeamScore;
+            
+            if (!(isAwayTeam || isHomeTeam))
+                continue;
+            
+            if ((isHomeTeam && homeTeamWon) || (isAwayTeam && awayTeamWon)) {
+                sumOfPoints += 3;
+            } else if (draw) {
+                sumOfPoints += 1;
+            }
+        }
+        
+        return sumOfPoints;
+    }
+    
+}
