3da572cf0dc7c8601735b67107ef5326ff8f3dc9
R2D2 <artur0y95@gmail.com>
2025-12-13 21:31:46 +0100

    dla klasy Square zdefiniowano pole instancji side, konstruktory, gettery, metody statyczne oraz przesłoniętą metodę toString

---
+++ cw09/src/Square.java
@@ -0,0 +1,54 @@
+public class Square {
+    
+    private double side;
+    
+    public Square(double side) {
+        this.side = side;
+    }
+    
+    public Square() {
+        this(1);
+    }
+    
+    public static double getTotalArea(Square[] sqs) {
+        double sumOfSqsAreas = 0;
+        for (Square s : sqs) {
+            sumOfSqsAreas += s.getArea();
+        }
+        return sumOfSqsAreas;
+    }
+    
+    public static Square getMax(Square[] sqs) {
+        int maxSquareIndex = 0;
+        for (int i = 1; i < sqs.length; i++) {
+            if (sqs[i].getSide() > sqs[maxSquareIndex].getSide())
+                maxSquareIndex = i;
+        }
+        return sqs[maxSquareIndex];
+    }
+    
+    public double getSide() {
+        return side;
+    }
+    
+    public double getArea() {
+        return Math.pow(side, 2);
+    }
+    
+    public double getDiagonal() {
+        return side * Math.sqrt(2);
+    }
+    
+    public double getPerimeter() {
+        return side * 4;
+    }
+    
+    public Square getDoubled() {
+        return new Square(side * 2);
+    }
+    
+    @Override
+    public String toString() {
+        return "Square[" + side + "]";
+    }
+}
