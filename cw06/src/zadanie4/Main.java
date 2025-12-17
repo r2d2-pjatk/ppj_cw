0072ef70325876f3045ef1652db1fe0d7a3ee893
R2D2 <artur0y95@gmail.com>
2025-12-17 19:28:32 +0100

    modyfikacja blędnegp wyrażenia z Math.random losujących liczbę kolumn i wierszy tablicy 2d

--- cw06/src/zadanie4/Main.java
+++ cw06/src/zadanie4/Main.java
@@ -10,8 +10,8 @@ public class Main {
         Wydrukuj w czytelnej postaci tablicę przed i po tej operacji.
         Tablica nie musi być prostokątna; nie wolno też tworzyć pomocniczych tablic.
         */
-        int rowNum = (int) (Math.random() * (5 - 3 + 1) + 2);
-        int colNum = (int) (Math.random() * (5 - 3 + 1) + 2);
+        int rowNum = (int) (Math.random() * (5 - 3 + 1) + 3);
+        int colNum = (int) (Math.random() * (5 - 3 + 1) + 3);
         int[][] arr = new int[rowNum][colNum];
         
         for (int i = 0; i < arr.length; i++) {
