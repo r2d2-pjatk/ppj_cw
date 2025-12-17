5401cf3c95e037fb6ec7c476ced0796232133759
R2D2 <artur0y95@gmail.com>
2025-12-17 20:04:18 +0100

    dodano puste linie między definicje funkcji

--- cw08/src/zadanie1/OneLiners.java
+++ cw08/src/zadanie1/OneLiners.java
@@ -1,6 +1,7 @@
 package zadanie1;
 
 public class OneLiners {
+    
     static void main(String[] args) {
         double a = 2, b = 3, c = 1;
         System.out.println("(a, b, c,)=(" + a + ", " +
@@ -47,13 +48,17 @@ public class OneLiners {
         // warunki: a != 0, delta > 0
         return (a != 0) && ((b * b - 4 * a * c) > 0);
     }
+    
     public static boolean monot(double a, double b, double c) {
         return (b > a && c > b) || (a > b & b > c);
     }
+    
     public static double maxEl(double a, double b, double c) {
         return (a > b && a > c) ? a : ((b > a && b > c) ? b : c);
     }
+    
     public static int numPos(double a, double b, double c) {
         return (a > 0 ? 1 : 0) + (b > 0 ? 1 : 0) + (c > 0 ? 1 : 0);
     }
+    
 }
