3464736a39fe982a295fd0f2951dfa58e31430d9
R2D2 <artur0y95@gmail.com>
2025-12-28 22:02:02 +0100

    dodano komentarz z poleceniem zadania

--- cw03/src/zadanie_dodatkowe1/Main.java
+++ cw03/src/zadanie_dodatkowe1/Main.java
@@ -3,9 +3,21 @@ package zadanie_dodatkowe1;
 
 public class Main {
     static void main() {
-        int number = 456;
+        /*
+        Korzystając wyłącznie z operacji matematycznych, napisz program,
+        który pobiera 3-cyfrową liczbę naturalną, niebędącą wielokrotnością 10,
+        następnie wyprowadza na wyjściu liczbę składającą się z cyfr danej liczby w odwrotnej kolejności (np. 123 -> 321).
+        Dane wejściowe podaj w inicjalizacji odpowiedniej zmiennej (typu int) w programie.
+        Jako dodatkowe wyzwanie możesz spróbować przerobić program w taki sposób,
+        żeby działał dla dowolnej ilości cyfr w liczbie :)
+        */
+        int number = 123;
         
-        int reversedNumber = ((number % 10) * 100) + ((number % 100) - (number % 10)) + ((number - (123 % 100))/100);
+        int hundredsDigit = number % 10;
+        int tensDigit = ((number % 100) - hundredsDigit) / 10;
+        int onesDigit = (number - tensDigit * 10 - hundredsDigit) / 100;
+        
+        int reversedNumber = hundredsDigit * 100 + tensDigit * 10 + onesDigit;
         IO.println(reversedNumber);
     }
 }
